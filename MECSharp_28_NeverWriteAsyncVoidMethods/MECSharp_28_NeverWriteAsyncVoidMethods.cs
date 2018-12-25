b4e7f90d4c878a8c93d1a47fdbae8caa01d79142
henriquegaia <henrique.gaia@gmail.com>
2018-12-25 13:48:52 +0100

    28, Pg146_1_CustomAwaitableTypes

--- MECSharp_28_NeverWriteAsyncVoidMethods/MECSharp_28_NeverWriteAsyncVoidMethods.cs
+++ MECSharp_28_NeverWriteAsyncVoidMethods/MECSharp_28_NeverWriteAsyncVoidMethods.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MECSharp_28_NeverWriteAsyncVoidMethods
+﻿namespace MECSharp_28_NeverWriteAsyncVoidMethods
 {
     class MECSharp_28_NeverWriteAsyncVoidMethods
     {
         static void Main(string[] args)
         {
             //Pg144_2_UseAppDomain.Test();
-            Pg146_2_AsyncVoidEventHandler.Test();
+            //Pg146_2_AsyncVoidEventHandler.Test();
+            //Pg146_1_CustomAwaitableTypes.Test();
         }
     }
 }
---
+++ MECSharp_28_NeverWriteAsyncVoidMethods/Pg146_1_CustomAwaitableTypes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECSharp_28_NeverWriteAsyncVoidMethods
+{
+    class Pg146_1_CustomAwaitableTypes
+    {
+        public static async void Test()
+        {
+        }
+    }
+
+    class Awaitable
+    {
+        public Awaiter GetAwaiter() => new Awaiter();
+
+        //public static async void Foo() => await Bar();
+
+        //public static async Awaitable Bar()
+        //{
+        //    Awaitable awaitable = new Awaitable();
+        //    await awaitable;
+        //    return awaitable.GetAwaiter();
+        //}
+    }
+
+    class Awaiter : INotifyCompletion
+    {
+        public void OnCompleted(Action continuation)
+        {
+        }
+
+        public bool IsCompleted { get; }
+        public void GetResult()
+        {
+        }
+    }
+}
