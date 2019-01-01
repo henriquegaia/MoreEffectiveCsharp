bdac77651fa20fafc6db785ea8a925e51aa59510
henriquegaia <henrique.gaia@gmail.com>
2019-01-01 13:36:35 +0100

    Create i 31

---
+++ MECSharp_31_AvoidMarshallingContextUnnecess/MECSharp_31_AvoidMarshallingContextUnnecess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MECSharp_31_AvoidMarshallingContextUnnecess
+{
+    class MECSharp_31_AvoidMarshallingContextUnnecess
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentContext);
+        }
+    }
+}
