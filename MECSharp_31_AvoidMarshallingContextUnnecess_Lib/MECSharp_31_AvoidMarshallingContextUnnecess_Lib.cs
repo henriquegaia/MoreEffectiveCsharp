f1d915434d02f9f83706e67e67e82a939d0c9a99
henriquegaia <henrique.gaia@gmail.com>
2019-01-05 14:09:57 +0100

    i31 pg 157

--- MECSharp_31_AvoidMarshallingContextUnnecess_Lib/MECSharp_31_AvoidMarshallingContextUnnecess_Lib.cs
+++ MECSharp_31_AvoidMarshallingContextUnnecess_Lib/MECSharp_31_AvoidMarshallingContextUnnecess_Lib.cs
@@ -2,36 +2,48 @@
 using System.Threading;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Utilities;
 
 namespace MECSharp_31_AvoidMarshallingContextUnnecess_Lib
 {
     public static class MECSharp_31_AvoidMarshallingContextUnnecess_Lib
     {
-        public static void RunTests()
+        public static async void RunTests()
         {
             // 156_3: what's marshalling ---------------------------------------
 
-            WhatIsMarshalling();
+            _156_3_WhatIsMarshalling();
 
             // 156_3: keep track of current ctx --------------------------------
             // Thread.CurrentContext vs Synchronization Context
 
-            KeepTrackOfContext();
+            //_156_3_KeepTrackOfContext();
 
             // 156_3: examples of ctx aware and ctx free code
             // 156_4: execute ctx aware code on the wrong context --------------
             // 156_5: ?
             // 157  : code
+
+            string s = await _157_AvoidRunningCtxAwareOnContextFree();
+
             // 158  : code
         }
 
-        public static void WhatIsMarshalling()
+        private static async Task<string> _157_AvoidRunningCtxAwareOnContextFree()
+        {
+            var res = await AsyncAndThreading.ReadFileAsync(true);
+            return res;
+        }
+
+        public static void _156_3_WhatIsMarshalling()
         {
             ComClass com = new ComClass();
             bool isCom = Marshal.IsComObject(com);
         }
 
-        public static void KeepTrackOfContext()
+        public static void _156_3_KeepTrackOfContext()
         {
             var ct = Thread.CurrentContext;
             var dc = Context.DefaultContext;
--- Utilities/AsyncAndThreading.cs
+++ Utilities/AsyncAndThreading.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -31,12 +33,28 @@ namespace Utilities
             {
                 await task;
             }
-            catch (Exception ex) when (onErrors(ex)) 
-            {}
+            catch (Exception ex) when (onErrors(ex))
+            { }
         }
 
-        public static int ThreadCount() => 
+        public static int ThreadCount() =>
             Process.GetCurrentProcess().Threads.Count;
+
+        public static async Task<string> ReadFileAsync(
+            bool continueOnCapturedContext, string filename = @"C:\Users\Henrique-private\Desktop\test2.txt")
+        {
+            byte[] result;
+
+            using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+            {
+                result = new byte[SourceStream.Length];
+                await SourceStream
+                    .ReadAsync(result, 0, (int)SourceStream.Length)
+                    .ConfigureAwait(continueOnCapturedContext);
+            }
+
+            return Encoding.ASCII.GetString(result);
+        }
 
         public static ExecutionContext GetExecutionContext()
         {
@@ -44,7 +62,7 @@ namespace Utilities
             return executionContext;
         }
 
-        public static int GetThreadId() => 
+        public static int GetThreadId() =>
             Thread.CurrentThread.ManagedThreadId;
     }
 
