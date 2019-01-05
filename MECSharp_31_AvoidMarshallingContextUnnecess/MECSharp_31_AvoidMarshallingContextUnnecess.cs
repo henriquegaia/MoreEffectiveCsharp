506881369d31df43aa8b5527d2ed35e6a66d8320
henriquegaia <henrique.gaia@gmail.com>
2019-01-05 06:55:16 +0100

    i31 questions

--- MECSharp_31_AvoidMarshallingContextUnnecess/MECSharp_31_AvoidMarshallingContextUnnecess.cs
+++ MECSharp_31_AvoidMarshallingContextUnnecess/MECSharp_31_AvoidMarshallingContextUnnecess.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MECSharp_31_AvoidMarshallingContextUnnecess
 {
@@ -11,7 +7,15 @@ namespace MECSharp_31_AvoidMarshallingContextUnnecess
     {
         static void Main(string[] args)
         {
+            // 156_3: what's marshalling
+            // 156_3: keep track of current ctx
             Console.WriteLine(Thread.CurrentContext);
+            // 156_3: examples of ctx aware and ctx free code
+            // 156_4: ?
+            // 156_5: ?
+            // 157  : code
+            // 158  : code
+
         }
     }
 }
