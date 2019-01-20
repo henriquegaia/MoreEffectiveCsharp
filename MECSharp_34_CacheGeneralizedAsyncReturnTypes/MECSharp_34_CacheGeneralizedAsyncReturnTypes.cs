e2b5fe81e245e21d92dd46a285a30f8c8fb2f16d
henriquegaia <henrique.gaia@gmail.com>
2019-01-20 08:21:35 +0100

    Start i34 CacheGeneralizedAsyncReturnTypes

---
+++ MECSharp_34_CacheGeneralizedAsyncReturnTypes/MECSharp_34_CacheGeneralizedAsyncReturnTypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MECSharp_34_CacheGeneralizedAsyncReturnTypes
+{
+    class MECSharp_34_CacheGeneralizedAsyncReturnTypes
+    {
+        static DateTime lastReading = DateTime.Now - TimeSpan.FromMinutes(6);
+        static List<WeatherData> recentObservations = new List<WeatherData>();
+
+        static void Main(string[] args)
+        {
+        }
+
+        static ValueTask<IEnumerable<WeatherData>> RetHistDataAsync()
+        {
+            if (DateTime.Now - lastReading > TimeSpan.FromMinutes(5))
+            {
+                return new ValueTask<IEnumerable<WeatherData>>(recentObservations);
+            }
+
+            //var obs = await RetObsData();
+            //recentObservations.Add(obs);
+            return new ValueTask<IEnumerable<WeatherData>>(recentObservations);
+        }
+
+        static async Task<WeatherData> RetObsData() => await Task.Run(() => new WeatherData());
+    }
+
+    class WeatherData
+    {
+
+    }
+}
