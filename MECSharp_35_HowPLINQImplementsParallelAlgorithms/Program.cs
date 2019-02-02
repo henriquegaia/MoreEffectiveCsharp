b19220012955949d1a7b88abf4764c338228b23a
henriquegaia <henrique.gaia@gmail.com>
2019-02-02 13:52:53 +0100

    Create item 35 MECSharp_35_HowPLINQImplementsParallelAlgorithms

---
+++ MECSharp_35_HowPLINQImplementsParallelAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MECSharp_35_HowPLINQImplementsParallelAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
