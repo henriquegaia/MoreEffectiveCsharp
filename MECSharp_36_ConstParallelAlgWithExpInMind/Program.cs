bc0e953279395667a359ba67b58827a391a8a7ee
henriquegaia <henrique.gaia@gmail.com>
2019-02-16 13:34:32 +0100

    i36 with vs code

---
+++ MECSharp_36_ConstParallelAlgWithExpInMind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MECSharp_36_ConstParallelAlgWithExpInMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
